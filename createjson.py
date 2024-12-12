c99e64ffa62e667bc046e2063a2a56f55ffc692d
Mike Cook <mike@possibilityspace.org>
2024-12-12 17:48:29 +0000

    fix to last-train name overlap

--- createjson.py
+++ createjson.py
@@ -1,15 +1,15 @@
 import os, json
 
-jsonobj = {"_base":"https://raw.githubusercontent.com/gamesbyangelina/strudel/main/"};
-
-
+directory = {"_base":"https://raw.githubusercontent.com/gamesbyangelina/strudel/main/"};
 
 for file in os.listdir("."):
     if file.endswith(".mp3"):
         cat = file.split('-')[0]
-        if not cat in jsonobj:
-            jsonobj[cat] = []
-        jsonobj[cat].append(file)
+        if not cat in directory:
+            directory[cat] = []
+        directory[cat].append(file)
+        #hello i am lazy
+        directory[cat] = sorted(directory[cat])
         
 with open("strudel.json", "w") as f:
-    f.write(json.dumps(jsonobj))
+    f.write(json.dumps(directory))
