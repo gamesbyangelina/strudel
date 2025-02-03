d2ab860051fdd644e5da735a1b6ab6aaf871f06d
Mike Cook <mike@possibilityspace.org>
2025-02-03 21:03:53 +0000

    restructure

--- createjson.py
+++ createjson.py
@@ -2,14 +2,19 @@ import os, json
 
 directory = {"_base":"https://raw.githubusercontent.com/gamesbyangelina/strudel/main/"};
 
-for file in os.listdir("."):
-    if file.endswith(".mp3"):
-        cat = file.split('-')[0]
-        if not cat in directory:
-            directory[cat] = []
-        directory[cat].append(file)
-        #hello i am lazy
-        directory[cat] = sorted(directory[cat])
-        
+for subdir, dirs, files in os.walk("."):
+    for file in files:
+        if file.endswith(".mp3"):
+            # print(subdir[2:])
+            # print(file)
+            cat = subdir[2:]
+            if not cat in directory:
+                directory[cat] = []
+            directory[cat].append(subdir[2:]+"/"+file)
+            #hello i am lazy
+            directory[cat] = sorted(directory[cat])
+
 with open("strudel.json", "w") as f:
     f.write(json.dumps(directory))
+
+exit()
